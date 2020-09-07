0bc8277d40913d0bd5d90ff4fb24333aa81d0233
nataliiatkachenko <azaria1993@gmail.com>
2020-09-07 22:25:06 +0300

    add dbinitializer

---
+++ WebForumTestTask/Models/ThemeDbInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Test_Task.Models;
+
+namespace WebForumTestTask.Models
+{
+    public class ThemeDbInitializer : CreateDatabaseIfNotExists<ForumContext>
+    {
+        protected override void Seed(ForumContext db)
+        {
+            db.Themes.Add(new Theme { Id = 1, Title = "Test" });
+
+            base.Seed(db);
+        }
+    }
+}
