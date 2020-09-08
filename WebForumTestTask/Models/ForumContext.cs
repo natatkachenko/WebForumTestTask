46b09b44ddcd4e7884cf1f862e742bb7019885ef
nataliiatkachenko <azaria1993@gmail.com>
2020-09-08 13:44:47 +0300

    added Details method in HomeController, Details View

--- WebForumTestTask/Models/ForumContext.cs
+++ WebForumTestTask/Models/ForumContext.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 
-namespace Test_Task.Models
+namespace WebForumTestTask.Models
 {
     public class ForumContext : DbContext
     {
--- WebForumTestTask/Models/ThemeDbInitializer.cs
+++ WebForumTestTask/Models/ThemeDbInitializer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-using Test_Task.Models;
+using WebForumTestTask.Models;
 
 namespace WebForumTestTask.Models
 {
